360b0472cceae7ec3673d0866c64347f184e31b2
M Murdaca <m.murdaca1@studenti.uniba.it>
2023-04-18 19:45:31 +0200

    simple test #1

--- src/test/java/JavaTest/Simple_Test.java
+++ src/test/java/JavaTest/Simple_Test.java
@@ -1,6 +1,36 @@
 package JavaTest;
 
+import JavaCode.CreateStrings;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Simple_Test {
 
+    @Test
+    @DisplayName("CreateString")
+    void simpleCase() {
 
+        CreateStrings createStrings = new CreateStrings();
+
+        Assertions.assertEquals("PNN123456LTT", createStrings.creaStringa("panna", "123456","ltt"));
+    }
+
+    @Test
+    @DisplayName("CreateString2")
+    void simpleCase2() {
+
+        CreateStrings createStrings = new CreateStrings();
+
+        Assertions.assertEquals("PCC123456LTT", createStrings.creaStringa("paaaaacaaccaa", "123456","ltt"));
+    }
+
+    @Test
+    @DisplayName("CreateString3")
+    void simpleCase3() {
+
+        CreateStrings createStrings = new CreateStrings();
+
+        Assertions.assertEquals("PAC123456LTT", createStrings.creaStringa("paaaaacaaaaaaaa", "123456","ltt"));
+    }
 }
